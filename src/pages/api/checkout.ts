66ff857147cc21fefb2f4d6b2a7b3dfc909fde8e
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2023-12-22 22:05:50 -0300

    Product and Checkout

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from '../../lib/stripe';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const priceId = 'price_1OQJarJjMAtibRf0DwEyKQxD';
+
+  const successUrl = `${process.env.NEXT_URL}/success`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'subscription',
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  });
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  });
+}
